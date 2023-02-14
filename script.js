46725a0bab888b8ea4d216528e09158ba49de30a
ngntrgduc <trungducnguyen1511@gmail.com>
2023-02-14 20:30:29 +0700

    Fix bug
    Change word to look up to lower case

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var searchBox = document.getElementById("searchBox");
 searchBox.addEventListener("keydown", function (e) {
     if (e.key === 'Enter') { 
-        link = "https://www.oxfordlearnersdictionaries.com/definition/english/" + e.target.value;
+        link = "https://www.oxfordlearnersdictionaries.com/definition/english/" + e.target.value.toLowerCase();
         window.open(link, '_blank');
     }
 });
