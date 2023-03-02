40b0a4aca12b0af73ab1f5179731e52caac55ea5
ngntrgduc <trungducnguyen1511@gmail.com>
2023-03-02 18:16:38 +0700

    Add feature: can look up to phrasal verb

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var searchBox = document.getElementById("searchBox");
 searchBox.addEventListener("keydown", function (e) {
     if (e.key === 'Enter') { 
-        link = "https://www.oxfordlearnersdictionaries.com/definition/english/" + e.target.value.toLowerCase();
-        window.open(link, '_blank');
+        let word = e.target.value.toLowerCase().trim().replaceAll(" ", "-");
+        link = "https://www.oxfordlearnersdictionaries.com/definition/english/" + word;
+        window.open(link, '_blank');    
     }
 });
