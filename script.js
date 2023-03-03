c1e45d8787024bb283cdf6cf15d223cbbd0a8f50
ngntrgduc <trungducnguyen1511@gmail.com>
2023-03-03 22:01:28 +0700

    Update extension
    Can look up with 2 dictionaries, change style

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-var searchBox = document.getElementById("searchBox");
+let searchBox = document.getElementById("searchBox");
 searchBox.addEventListener("keydown", function (e) {
-    if (e.key === 'Enter') { 
+    if (e.key === 'Enter') {
         let word = e.target.value.toLowerCase().trim().replaceAll(" ", "-");
-        link = "https://www.oxfordlearnersdictionaries.com/definition/english/" + word;
+        let dictionary = document.getElementById("dictionary").value;
+        if (dictionary == "Cambridge") {
+            link = "https://dictionary.cambridge.org/dictionary/english/" + word;
+        } else {
+            link = "https://www.oxfordlearnersdictionaries.com/definition/english/" + word;
+        }
         window.open(link, '_blank');    
     }
 });
