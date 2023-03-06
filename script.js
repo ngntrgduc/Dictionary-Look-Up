2e0621a618ae01761caf9dc6b03b0968ed4dc8e1
ngntrgduc <trungducnguyen1511@gmail.com>
2023-03-06 22:26:53 +0700

    Add feature: change dictionary via Tab key

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-let searchBox = document.getElementById("searchBox");
-searchBox.addEventListener("keydown", function (e) {
+document.querySelector('select').addEventListener('focus', function () {
+    let selectedDictionary = document.getElementById("dictionary").value;
+    if (selectedDictionary == "Cambridge") {
+        document.getElementById('dictionary').value = "Oxford";
+    } else {
+        document.getElementById('dictionary').value = "Cambridge";
+    }
+    // Auto focus to search box after changed dictionary
+    document.querySelector('input').focus(); 
+});
+
+document.getElementById("searchBox").addEventListener("keydown", function (e) {
     if (e.key === 'Enter') {
         let word = e.target.value.toLowerCase().trim().replaceAll(" ", "-");
-        let dictionary = document.getElementById("dictionary").value;
-        if (dictionary == "Cambridge") {
+        let selectedDictionary = document.getElementById("dictionary").value;
+        if (selectedDictionary == "Cambridge") {
             link = "https://dictionary.cambridge.org/dictionary/english/" + word;
         } else {
             link = "https://www.oxfordlearnersdictionaries.com/definition/english/" + word;
